6acc271cbe195f3248f0088dc3ce16006767d29f
JaeWoongHwang <jwngh15@gmail.com>
2018-01-05 06:29:44 +0000

    Final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :user_signed_in?
-  
+
   # 권한 부여
   def current_user
     # 현재 접속한 유저
